9f0def611de133521ee7c6eded68c9e78aced5f7
yanet-y <107769377+yanet-y@users.noreply.github.com>
2024-05-13 10:46:32 +0300

    Add files via upload

---
+++ Backend/src/test/java/com/Insa/Employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Insa.Employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
